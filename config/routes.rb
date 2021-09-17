9290e4cb29730193d35a56c14f22f0b4e890d2a8
Lisa Swart <arnoudlisa@gmail.com>
2021-09-17 18:59:11 -0400

    fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   get '/books', to: 'books#index'
   get '/books/:id', to: 'books#show'
   post '/books', to: 'books#create'
-  update '/books/:id', to: 'books#update'
+  patch '/books/:id', to: 'books#update'
   delete '/books/:id', to: 'books#destroy'
 end
