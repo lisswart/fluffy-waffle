724f8cf9f13286068b9b112983d0fbd5e93e596b
Lisa Swart <arnoudlisa@gmail.com>
2021-09-17 16:54:40 -0400

    fix mistakes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,34 +8,34 @@
 
 puts "clearing old data..."
 
-# User.destroy_all
+User.destroy_all
 
-# puts "seeding users..."
+puts "seeding users..."
 
-# 5.times do User.create(
-#     firstname: Faker::Name.first_name,
-#     lastname: Faker::Name.last_name,
-#     username: Faker::Twitter.unique.screen_name,
-#     email: Faker::Internet.unique.email,
-#     password_digest: Faker::Internet.unique.password
-#   )
+5.times do User.create!(
+    firstname: Faker::Name.first_name,
+    lastname: Faker::Name.last_name,
+    username: Faker::Twitter.unique.screen_name,
+    email: Faker::Internet.unique.email,
+    password_digest: Faker::Internet.unique.password
+  )
 
-# Book.destroy_all
+Book.destroy_all
 
-# puts "seeding books..."
+puts "seeding books..."
 
-# 50.times do Book.create(
-#   title: Faker::Book.title,
-#   author: Faker::Name.name
-#   )
-# end
+50.times do Book.create!(
+  title: Faker::Book.title,
+  author: Faker::Name.name
+  )
+end
 
 UserBook.destroy_all
 
 puts "seeding UserBook..."
 
-75.times do UserBook.create(
-  book_id: rand(51..100),
+75.times do UserBook.create!(
+  book_id: rand(1..50),
   user_id: rand(1..5),
   description: Faker::Lorem.paragraph(sentence_count: 8),
   read_status: ["Not Begun", "In Progress", "Completed", "Abandoned"].sample,
